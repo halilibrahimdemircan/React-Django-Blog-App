81893c7d1b7f8090f79f9b6eb47135c73d89cc71
halilibrahimdemircan <58706397+halilibrahimdemircan@users.noreply.github.com>
2022-03-17 10:41:57 +0300

    morning

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div>Django React Blog App</div>
+    <Router></Router>
   );
 }
 
---
+++ client/src/router/AppRouter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+
+
+const AppRouter = () => {
+    return (
+        <div>Router</div>
+    )
+}
+
+export default AppRouter
